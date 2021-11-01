d1e4113c6458a56b299e0d11b4d80a1bf654a126
bona-098 <pencengirm@gmail.com>
2021-11-01 23:51:34 +0800

    as

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/home.dart';
+import 'package:flutter_application_1/screens/daftar.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
